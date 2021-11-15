da58a73b8953a2a26ba03b6bdfd7ebc72235b2cd
TomPixowl <62062520+TomPixowl@users.noreply.github.com>
2021-11-15 20:35:56 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody playerBody;
+    [SerializeField] float speed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float rotationSpeed;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) Fire();
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerBody.velocity = transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerBody.velocity = -transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotationSpeed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * rotationSpeed, Space.World);
+        }
+    }
+
+    void Fire()
+    {
+        
+    }
+    void Jump()
+    {
+        playerBody.velocity = transform.up * jumpSpeed;
+    }
+}
+
+
